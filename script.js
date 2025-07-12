095b62366e4d9b0a5b438e6dc2fbaa32e09f5752
FreireCF <clecioferreira011@gmail.com>
2025-07-12 13:38:13 -0300

    modificações

--- script.js
+++ script.js
@@ -7,5 +7,26 @@ const form = document.getElementById('form')
 
 const sendForm = (event) =>{
   event.preventDefault()
+  const apiKey = apiKeyInput.value
+  const game = gameInput.value
+  const question = questionInput.value
+
+  console.log(apiKey, game, question)
+
+  if(apiKey == '' || game == '' || question == ''){
+    alert('Preencha todos os campos')
+    return
+  }
+
+  askButton.disabled = true
+  askButton.querySelector('.button-text').style.display = 'none'
+  askButton.querySelector('.loading').style.display = 'inline-block'
+
+  // Simula tempo de resposta
+  setTimeout(() => {
+    askButton.disabled = false
+    askButton.querySelector('.button-text').style.display = 'inline'
+    askButton.querySelector('.loading').style.display = 'none'
+  }, 3000)
 }
 form.addEventListener('submit', sendForm)
